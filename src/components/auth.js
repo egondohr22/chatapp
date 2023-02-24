eb6ca0cb44277521c8641c8eb208e00c33b4761c
Egon Dohr <egondohr03@gmail.com>
2023-02-24 12:23:35 +0200

    Google auth and sign out

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,13 +1,32 @@
-import { auth } from '../config/firebase';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, googleProvider } from '../config/firebase';
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { useState } from 'react';
 
 export const Auth = () => {
     const [email, setEmail] = useState("");   
     const [password, setPassword] = useState("");    
     const signIn = async () => {
-        await createUserWithEmailAndPassword(auth, email, password);
+        try {
+          await createUserWithEmailAndPassword(auth, email, password);
+        } catch(err) {
+            console.error(err);
+        }
     };
+    const googleSingIn = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider);
+          } catch(err) {
+              console.error(err);
+          }
+    };
+    const logout = async () => {
+        try {
+            await signOut(auth);
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
     return (
         <div>
             <input 
@@ -21,6 +40,9 @@ export const Auth = () => {
                 onChange={(e) => setPassword(e.target.value)}
             />
             <button onClick={signIn}> Sign In </button>
+
+            <button onClick={googleSingIn}>Sign in with Google</button>
+            <button onClick={logout}>Logout</button>
         </div>
     )
 }
